dc78bf16b34a9b1299e1b4e6b489c816e117614d
abdul subhan <abdulsubhan6535@gmail.com>
2025-11-04 21:20:54 +0500

    Update basic_arithematic_operator.cpp

--- basic_arithematic_operator.cpp
+++ basic_arithematic_operator.cpp
@@ -12,9 +12,11 @@ int main()
     std::cout<<"The difference is "<<difference<<std::endl;
     std::cout<<"The multiple is "<<multiple<<std::endl;
     std::cout<<"The quotient is "<<Quotient;
-    //Output
-    //The sum is 25
-    //The difference is 15
-    //The multiple is 100
-    //The quotient is 4 
-}
+    reurn 0;
+}    
+//Output
+//The sum is 25
+//The difference is 15
+//The multiple is 100
+//The quotient is 4 
+
